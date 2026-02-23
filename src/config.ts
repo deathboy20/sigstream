aa747da258eb37b52a969cb547f4cab25a6bf846
Isaac Obeng <139678869+isaacobenzy@users.noreply.github.com>
2026-02-23 12:58:07 +0000

    heat

--- src/config.ts
+++
@@ -1,2 +0,0 @@
-// export const STREAM_API_URL = 'http://localhost:3001';
-export const STREAM_API_URL = 'https://stream-server-fxu5.onrender.com';
--- src/utils/conferenceSocket.ts
+++
@@ -1,6 +0,0 @@
-import { io } from 'socket.io-client';
-import { STREAM_API_URL } from '../config';
-
-export const conferenceSocket = io(STREAM_API_URL, {
-  autoConnect: true
-});
